4bf79cb49dd9e166707fab5a6ceb227dd17b10c5
Germy <jalee@cmu.edu>
2017-05-05 16:45:37 -0400

    precompiled assets

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160309140307) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "assignments", force: :cascade do |t|
     t.integer "store_id"
     t.integer "employee_id"
