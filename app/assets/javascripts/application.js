e16d8951588d8daac8a8660170ac96ca00a45475
Germy <jalee@cmu.edu>
2017-05-05 16:52:46 -0400

    added precompile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation
-//= require foundation/foundation.abide
 //= require_tree .
 //= require jquery_nested_form
 //= require foundation-datetimepicker
